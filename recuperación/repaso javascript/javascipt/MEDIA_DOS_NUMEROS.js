d2fb4d9a7a77645bfadc0f2ca63db3d5173eb2e2
alexcampodarbe <acamjim2107@g.educaand.es>
2025-05-12 14:26:28 +0200

    empezamos con javascript

---
+++ recuperación/repaso javascript/javascipt/MEDIA_DOS_NUMEROS.js
@@ -0,0 +1,8 @@
+
+const num1 = parseInt(prompt("Escribe un número: "));
+const num2 = parseInt(prompt("Escribe otro número: "));
+
+const suma = num1 + num2;
+const division = suma / 2;
+
+alert("La media aritmética de "+ num1 + " y " + num2 + " es " + division);
---
+++ recuperación/repaso javascript/javascipt/script.js
@@ -0,0 +1,25 @@
+
+let variable = 5; //declaracion de variables
+
+texto = document.querySelector("p");
+
+texto.addEventListener("click", function(){
+    alert("Hola mundo");
+});
+
+
+
+// console.log: es como el print de python pero ne el navegador.nos permite:
+//Visualizar variables y objetos
+console.log("hola mundo"); //objeto
+console.log(variable);
+
+//Visualizar cadenas de texto
+console.log("hola, que tal");
+
+//Visualizar operaciones matemáticas mostrando el resultado
+console.log(5*3+28%3);
+
+//Funcion alert: print de python pero en una pestaña emergente. Nos permite:
+
+//Visualizar
