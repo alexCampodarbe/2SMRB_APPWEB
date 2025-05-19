2d4fe138b76b246fae87874630a81f93042fee6d
alexCampodarbe <acamjim2107@g.educaand.es>
2025-05-19 19:18:15 +0200

    toda la tarea 3 acabada

---
+++ recuperación/repaso javascript/javascipt/ANYOS_BISIESTOS.js
@@ -0,0 +1,15 @@
+
+const anyo = parseInt(prompt("Escriba un año y le diré si es bisiesto: "));
+
+if (anyo % 4 == 0){
+    if (anyo % 100 == 0){
+        if (anyo % 400 == 0){
+            alert("El año " + anyo + " es un año bisiesto porque es múltiplo de 400.");
+        } else {
+            alert("El año " + anyo + " no es un año bisiesto porque es múltiplo de 100 sin ser múltiplo de 400");  
+        }
+    } 
+
+} else {
+    alert("El año " + anyo + " no es un año bisiesto porque no es múltiplo de 4.")
+}
--- recuperación/repaso javascript/javascipt/COMPARADOR_MULTIPLOS.js
+++ recuperación/repaso javascript/javascipt/COMPARADOR_MULTIPLOS.js
@@ -2,12 +2,28 @@
 const n1 = parseInt(prompt(" Escriba un número: "));
 const n2 = parseInt(prompt(" Escriba otro número: "));
 
-if (n1){
 
-} else if (n1 >= n2){
-    if (n1 % n2 == 0){
+
+
+
+if (n1 >= n2){
+    if (n2 == 0){
+    alert("Lo siento, este programa no admite valores negativos");
+    } else{
+        if (n1 % n2 == 0){
         alert(n1 + " es múltiplo de " + n2);
-    }else{
+        }else{
         alert(n1 + " no es múltiplo de " + n2);
+        }
+    }
+}else{
+    if (n1 == 0){
+    alert("Lo siento, este programa no admite valores negativos");
+    }else{
+        if (n2 % n1 ==0){
+        alert(n2 + " es múltiplo de " + n1);
+        } else{
+        alert(n2 + " no es múltiplo de " + n1);
+        }
     }
 }
