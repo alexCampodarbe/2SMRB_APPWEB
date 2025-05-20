e4ee8f2d048672185484cee112a5439b96fa5fde
alexcampodarbe <acamjim2107@g.educaand.es>
2025-05-20 11:57:08 +0200

    ejercicios tarea 3 corregidos

--- recuperación/repaso javascript/javascipt/COMPARADOR_MULTIPLOS.js
+++ recuperación/repaso javascript/javascipt/COMPARADOR_MULTIPLOS.js
@@ -7,8 +7,11 @@ const n2 = parseInt(prompt(" Escriba otro número: "));
 
 
 if (n1 >= n2){
-    if (n2 == 0){
-    alert("Lo siento, este programa no admite valores negativos");
+    if (n2 == 0 || n1 == 0){
+        alert("Lo siento, este programa no admite valores nulos");
+    
+    } else if(n1 < 0 || n2 < 0){
+        alert("Lo siento, este programa no admite valores negativos");
     } else{
         if (n1 % n2 == 0){
         alert(n1 + " es múltiplo de " + n2);
@@ -17,8 +20,11 @@ if (n1 >= n2){
         }
     }
 }else{
-    if (n1 == 0){
-    alert("Lo siento, este programa no admite valores negativos");
+    if (n1 == 0 || n2 == 0){
+        alert("Lo siento, este programa no admite valores nulos");
+    
+    } else if(n2 < 0 || n1 < 0){
+        alert("Lo siento, este programa no admite valores negativos");
     }else{
         if (n2 % n1 ==0){
         alert(n2 + " es múltiplo de " + n1);
