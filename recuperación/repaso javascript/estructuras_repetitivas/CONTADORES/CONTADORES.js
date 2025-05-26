fc1462909118317e8bd5c2f0806956a4fd09e8ef
alexCampodarbe <acamjim2107@g.educaand.es>
2025-05-26 11:34:07 +0200

    intento en casa

--- recuperación/repaso javascript/estructuras_repetitivas/CONTADORES/CONTADORES.js
+++ recuperación/repaso javascript/estructuras_repetitivas/CONTADORES/CONTADORES.js
@@ -1,13 +1,30 @@
 
 
-const n1= parseInt(prompt("¿Cuántos valoras vas a introducir?"));
+// const n1= parseInt(prompt("¿Cuántos valoras vas a introducir?"));
+// let i=1
+// if(n1 <=0){
+//     alert("¡IMPOSIBLE!")
+// } else if(n1 ==0){
+//     alert("has escrito el número 0, prueba otro numero para que te lo detecto borrico");
+// } else{
+//     for(let i=1 ;i<=n1; i++){
+//         const n = parseInt(prompt("Escribe el número " + i + ": "));
+//         if (n <= 0){
+//                 const total = i + 0;
+//                 alert("Ha escrito "+ total + " números negativos");
+//             }
+//         }
+// }
 
-if(n1 <=0){
-    alert("¡IMPOSIBLE!")
-} else if(n1 ==0){
-    alert("has escrito el número 0, prueba otro numero para que te lo detecto borrico");
-} else{
-    for(let i=1; i=n1;i++){
-        let n =
+let c = parseInt(prompt("¿Cuántos valores va a introducir?")), n = 0;
+
+if (c < 0) {
+    alert("¡Imposible!");
+} else if (c === 0) {
+    alert("No ha escrito ningún número negativo.");
+} else {
+    for (let i = 1; i <= c; i++) {
+        if (parseInt(prompt("Escriba el número "+ i +": ")) < 0) n++;
     }
+    alert("Ha escrito "+ n +" números negativos.");
 }
