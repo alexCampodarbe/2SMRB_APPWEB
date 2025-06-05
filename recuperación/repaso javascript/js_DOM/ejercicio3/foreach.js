2abe890e7b94b7a0b1225d7e91ca2317a5adb537
alexcampodarbe <acamjim2107@g.educaand.es>
2025-06-05 13:27:16 +0200

    hola

--- recuperación/repaso javascript/js_DOM/ejercicio3/foreach.js
+++ recuperación/repaso javascript/js_DOM/ejercicio3/foreach.js
@@ -1,10 +1,13 @@
 
-let lista = document.querySelectorAll(".lista");
+let lista = document.querySelectorAll(".lista_contenido");
 let salida_lista =[]
+// let lista_contenido = document.querySelectorAll(".lista_contenido")
 
-lista.forEach(lista =>{
-    salida_lista.push(lista.textContent);
+lista.forEach((elemento, i) =>{
+    // let lista_contenido = document.querySelectorAll(".lista_contenido")
+    elemento.textContent = "Nuevo elemento " + (i + 1);
+    // salida_lista.push(lista.textContent);
 })
 
-const espectaculo_de_errores = document.querySelector(".salida");
-espectaculo_de_errores.textContent = salida_lista;
+// const espectaculo_de_errores = document.querySelector(".salida");
+// espectaculo_de_errores.textContent = salida_lista;
