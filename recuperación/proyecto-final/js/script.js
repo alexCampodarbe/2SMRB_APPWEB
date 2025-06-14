7280a472e35baa2f017a416f9d56de774a3b1338
alexCampodarbe <acamjim2107@g.educaand.es>
2025-06-14 17:20:17 +0200

    trabajo final realizado

--- recuperación/proyecto-final/js/script.js
+++ recuperación/proyecto-final/js/script.js
@@ -11,8 +11,14 @@ const boton_principal = document.querySelector(".principal__seccion1__boton");
 const titulo_pie_lista = document.querySelectorAll(".pie_seccion__listas__titulo")
 const cuerpo = document.querySelector(".cuerpo");
 const principal = document.querySelector(".principal");
+const boton_siguiente = document.querySelector(".principal_seccion2_imagenes_figura_boton_derecha")
+const boton_atras = document.querySelector(".principal_seccion2_imagenes_figura_boton_izquierda");
+const rutas_imagenes = ["./assets/padre_de_familia.jpg", "./assets/imagen_batman.jpg", "./assets/simpsons.jpg"];
+const imgs = document.querySelector(".principal__seccion2__imagenes__figura__imagen")
 
 
+
+// funciones
 
 boton_oscuro.addEventListener("click", function(){
     cabecero.classList.toggle("cabecero_oscuro");
@@ -28,6 +34,11 @@ boton_oscuro.addEventListener("click", function(){
     titulo_pie_lista.forEach(objeto =>{
         objeto.classList.toggle("pie_seccion__listas__titulo_oscuro");
     })
+    if(boton_oscuro.textContent.trim() === "Modo claro"){ // Se utiliza el trim en esta comparacion porque a la hora de comparar con el if este no detecta los espacios por lo que con el trim eliminamos los espacios para poder compararlos
+        boton_oscuro.textContent = "Modo oscuro";
+    } else {
+        boton_oscuro.textContent = "Modo claro";
+    }
 })
 
 
@@ -36,4 +47,36 @@ boton_ampliar.addEventListener("click", function(){
     principal.classList.toggle("principal_ampliar");
     pie.classList.toggle("pie_ampliar")
     cuerpo.classList.toggle("cuerpo_ampliar")
+    if(boton_ampliar.textContent.trim() === "encoger"){ // Se utiliza el trim en esta comparacion porque a la hora de comparar con el if este no detecta los espacios por lo que con el trim eliminamos los espacios para poder compararlos
+        boton_ampliar.textContent = "ampliar";
+    } else {
+        boton_ampliar.textContent = "encoger";
+    }
 })
+
+let siguiente=0
+boton_siguiente.addEventListener("click",function(){
+    rutas_imagenes.forEach((elemento,lista) => {
+        if(lista == siguiente){
+            imgs.setAttribute("src",elemento)
+        }
+    });
+    siguiente++;
+    if(siguiente>=rutas_imagenes.length){
+        siguiente=0
+    }
+})
+
+let atras = 0;
+
+boton_atras.addEventListener("click", function(){
+    atras--;
+    if(atras < 0){
+        atras = rutas_imagenes.length - 1;
+    }
+    rutas_imagenes.forEach((elemento, lista) => {
+        if(lista === atras){
+            imgs.setAttribute("src", elemento);
+        }
+    });
+});
